137e20896e4b3bd10f5599db847e3d158a11acac
easydong19 <ymh1353@naver.com>
2022-11-08 18:39:32 +0900

    프로그래머스 LV0 day20~day24 연속된 수의 핪 smart풀이 추가

--- 코딩테스트입문/연속된수의합.py
+++ 코딩테스트입문/연속된수의합.py
@@ -16,3 +16,10 @@ def solution(num, total):
 
     return sorted(answer)
 
+def smart_solution(num, total):
+    answer = []
+    var = sum(range(num+1))
+    diff = total - var
+    start_num = diff//num
+    answer = [i+1+start_num for i in range(num)]
+    return answer
