0ee8dd53d26ca2b99d6ac37705c1943cf14422f3
easydong19 <ymh1353@naver.com>
2022-11-11 06:12:02 +0900

    파이썬 알고리즘 테스트 정규 표현식 정리

--- 파이썬알고리즘인터뷰/6장/Most_Common_word.py
+++ 파이썬알고리즘인터뷰/6장/Most_Common_word.py
@@ -1,10 +1,11 @@
-#6장 4번 가장 흔한 단어
+# 6장 4번 가장 흔한 단어
 import collections
 import re
 import string
 from collections import Counter
 
-#내가 푼 답
+
+# 내가 푼 답
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
         for char in string.punctuation:
@@ -13,12 +14,13 @@ class Solution:
         letter = [word for word in paragraph.lower().split() if not word in banned]
         return Counter(letter).most_common(1)[0][0]
 
-#책에서 푼 답
+
+# 책에서 푼 답
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
-        words = [word for word in re.sub(r'[^\w]',' ',paragraph)
-                 .lower().split()
-                        if word not in banned]
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+        .lower().split()
+                 if word not in banned]
 
         counts = collections.Counter(words)
         return counts.most_common(1)[0][0]
